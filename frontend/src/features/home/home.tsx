4444e64b82efb4d2e223ee36841180d3a11c86bb
expertyeti <leivetd@gmail.com>
2026-08-15 08:11:30 -0600

    modularizando el search bar de home y agregando efecto de scroll a el search bar

--- frontend/src/features/home/home.tsx
+++ frontend/src/features/home/home.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import Shared from '@shared';
 import type { ModItem } from './types';
 import { FeaturedMods } from './components/FeaturedMods';
 import { AllMods } from './components/AllMods';
 import { ModDetailsModal } from './components/ModDetailsModal';
+import { HomeSearchbar } from './components/HomeSearchbar';
 
 export const Home: React.FC = () => {
   const [selectedCard, setSelectedCard] = useState<ModItem | null>(null);
@@ -15,15 +15,7 @@ export const Home: React.FC = () => {
   return (
     <div className="items-center -m-8 justify-center text-white font-sans">
       <div className="relative">
-        <div className="sticky top-0 z-50 w-full">
-          <Shared.molecules.Searchbar 
-            placeholders={[
-              "Paste your favorite mod's ID...", 
-              "Search for mods...", 
-              "Search on GameBanana..."
-            ]} 
-          />
-        </div>
+        <HomeSearchbar />
         
         <div className="pt-2 sm:pt-8 px-8">
           <FeaturedMods />
